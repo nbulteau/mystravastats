72af03c12eb4586cc7c969a75e51c6db294eb068
Nicolas Bulteau <nicolas.bulteau@orange.com>
2020-11-29 20:45:43 +0100

    fix TUs

--- src/test/kotlin/me/nicolas/stravastats/core/business/FormatKtTest.kt
+++ src/test/kotlin/me/nicolas/stravastats/core/business/FormatKtTest.kt
@@ -8,8 +8,8 @@ internal class FormatKtTest {
     @Test
     fun `Int formatSeconds`() {
 
-        assertEquals("00m 00s", 0.formatSeconds())
-        assertEquals("00m 59s", 59.formatSeconds())
+        assertEquals("00s", 0.formatSeconds())
+        assertEquals("59s", 59.formatSeconds())
         assertEquals("01m 00s", 60.formatSeconds())
         assertEquals("02m 00s", 120.formatSeconds())
         assertEquals("59m 59s", 3599.formatSeconds())
@@ -18,14 +18,14 @@ internal class FormatKtTest {
     @Test
     fun `Double formatSeconds`() {
 
-        assertEquals("0:00", 0.0.formatSeconds())
-        assertEquals("0:59", 59.0.formatSeconds())
-        assertEquals("1:00", 60.0.formatSeconds())
-        assertEquals("2:00", 120.0.formatSeconds())
-        assertEquals("59:59", 3599.0.formatSeconds())
-        assertEquals("2:00", 120.99.formatSeconds())
-        assertEquals("2:00", 120.994.formatSeconds())
-        assertEquals("2:01", 120.995.formatSeconds())
-        assertEquals("59:59", 3599.994.formatSeconds())
+        assertEquals("0'00", 0.0.formatSeconds())
+        assertEquals("0'59", 59.0.formatSeconds())
+        assertEquals("1'00", 60.0.formatSeconds())
+        assertEquals("2'00", 120.0.formatSeconds())
+        assertEquals("59'59", 3599.0.formatSeconds())
+        assertEquals("2'00", 120.99.formatSeconds())
+        assertEquals("2'00", 120.994.formatSeconds())
+        assertEquals("2'01", 120.995.formatSeconds())
+        assertEquals("59'59", 3599.994.formatSeconds())
     }
 }
