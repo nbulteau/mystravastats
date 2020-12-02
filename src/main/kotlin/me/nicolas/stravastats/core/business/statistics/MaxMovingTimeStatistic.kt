146a96cb6287d770a839409b353b01852e2d6b95
Nicolas Bulteau <nicolas.bulteau@orange.com>
2020-12-02 08:41:58 +0100

    Add max moving time

---
+++ src/main/kotlin/me/nicolas/stravastats/core/business/statistics/MaxMovingTimeStatistic.kt
@@ -0,0 +1,22 @@
+package me.nicolas.stravastats.core.business.statistics
+
+import me.nicolas.stravastats.core.business.formatSeconds
+import me.nicolas.stravastats.infrastructure.dao.Activity
+
+internal class MaxMovingTimeStatistic(
+    activities: List<Activity>
+) : ActivityStatistic("Max moving time", activities) {
+
+    init {
+        activity = activities.maxByOrNull { it.movingTime }
+    }
+
+    override fun toString(): String {
+
+        return super.toString() + if (activity != null) {
+            "%s%s".format(activity?.movingTime?.formatSeconds(), activity)
+        } else {
+            " Not available"
+        }
+    }
+}
