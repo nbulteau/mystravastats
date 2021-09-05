5ae562f0c4dd1a301d3879824ee86d5753324f9b
Nicolas Bulteau <nicolas.bulteau@orange.com>
2021-09-05 17:13:42 +0200

    fix MacOS issue

--- src/main/kotlin/me/nicolas/stravastats/Utils.kt
+++ src/main/kotlin/me/nicolas/stravastats/Utils.kt
@@ -13,7 +13,6 @@ internal class OSValidator {
         var IS_WINDOWS = OS.indexOf("win") >= 0
         var IS_MAC = OS.indexOf("mac") >= 0
         var IS_UNIX = OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") > 0
-        var IS_SOLARIS = OS.indexOf("sunos") >= 0
     }
 }
 
@@ -34,9 +33,14 @@ fun openBrowser(url: String) {
             runtime.exec("rundll32 url.dll,FileProtocolHandler $url")
         }
         OSValidator.IS_MAC -> {
-            Desktop.getDesktop().browse(URI(url))
+            val uri = if(url.startsWith("http")) {
+                url
+            } else {
+                "file://$url"
+            }
+            Desktop.getDesktop().browse(URI(uri))
         }
-        else -> {
+        OSValidator.IS_UNIX -> {
             runtime.exec("xdg-open $url")
         }
     }
